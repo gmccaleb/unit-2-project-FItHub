ee936fc7db4a1f96d516212fb89b2316e16a1fa5
gmccaleb <gabriellemccaleb@gmail.com>
2026-02-20 23:23:33 -0600

    created spring boot app and linked to new database, set yp configurations

---
+++ FitHub/src/main/java/com/example/FitHub/FitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.FitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitHubApplication.class, args);
+	}
+
+}
---
+++ FitHub/src/test/java/com/example/FitHub/FitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
