f4a259c39fc2794a3a4c128abf00d5626b03f7a4
gmccaleb <gabriellemccaleb@gmail.com>
2026-02-24 23:19:41 -0600

    added workout and exercise models

---
+++ fithub-backend/src/main/java/com/example/FitHub/models/Exercise.java
@@ -0,0 +1,79 @@
+package com.example.FitHub.models;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "exercises")
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int workoutId;
+    private String exerciseName;
+    private int sets;
+    private int reps;
+    private int weight;
+
+    public Exercise() {
+    }
+
+    public Exercise(int id, int workoutId, String exerciseName, int sets, int reps, int weight) {
+        this.id = id;
+        this.workoutId = workoutId;
+        this.exerciseName = exerciseName;
+        this.sets = sets;
+        this.reps = reps;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getWorkoutId() {
+        return workoutId;
+    }
+
+    public void setWorkoutId(int workoutId) {
+        this.workoutId = workoutId;
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
